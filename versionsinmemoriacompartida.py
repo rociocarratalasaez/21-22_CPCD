7c1e020e3d0e9e39462a00253351217732eb4632
GuillermoMarquinez <105006688+GuillermoMarquinez@users.noreply.github.com>
2022-05-09 12:16:47 +0200

    Add files via upload
    He añadido el producto por columnas al programa final.

--- versionsinmemoriacompartida.py
+++ versionsinmemoriacompartida.py
@@ -12,7 +12,8 @@ import multiprocessing
 from multiprocessing import Process, Array
 from multiprocessing import Pool
 import time
- 
+
+#Producto de matrices cuadradas escritas como vector por filas 
 def prod_filas(a,b):
     num_filas=int(len(a)**(1/2))
     mul=[0]*len(a)
@@ -22,7 +23,7 @@ def prod_filas(a,b):
                     mul[i*num_filas+j]+=a[i*num_filas+k]*b[j+num_filas*k]
     return(mul)
     
-
+#Producto de matrices cuadradas escritas como vector por columnas
 def prod_columnas(a,b):
     num_filas=int(len(a)**(1/2))
     mul=[0]*len(a)
@@ -32,12 +33,14 @@ def prod_columnas(a,b):
                 mul[i+j*num_filas]+=a[i+k*num_filas]*b[j*num_filas+k]
     return(mul)
     
+#Suma de matrices escritas como vector por filas o columnas 
 def suma_matrices(a,b):
     suma=[0]*len(a)
     for i in range(0,len(a)):
         suma[i]=a[i]+b[i]    
     return(suma)
     
+#Generación de una matriz cuadrada aleatoria de n * n elementos escrita en un vector
 def matriz_aleatoria(n):
     a=[]
     for i in range(0,n*n):
@@ -45,19 +48,37 @@ def matriz_aleatoria(n):
         a.append(n)
     return(a)
 
+#Cambio del formato de la matriz, escrita por filas o columnas.
+def cambiar_forma_matriz(a):
+    b=[0]*len(a)
+    l=int(len(a)**(1/2))
+    for i in range(0,l):
+        for j in range(0,l):
+            b[l*i+j]=a[l*j+i]
+    return(b)
+            
+        
+#Programa principal para compara tiempos
 if __name__ == '__main__':
     
-    n=400
+    n=1000
     a=matriz_aleatoria(n)
     b=matriz_aleatoria(n)
-    sol=[0]*len(a)
+    aa=cambiar_forma_matriz(a)
+    bb=cambiar_forma_matriz(b)
     start1 = time.time()
     sol=prod_filas(a,b)
     end1=time.time()
     T1=end1-start1
-    print("El tiempo sin mutiprocessing es;"+ str(T1))
-
-    start2=time.time()
+    print("El tiempo por filas sin mutiprocessing es;"+ str(T1))
+    
+    start1 = time.time()
+    sol=prod_columnas(aa,bb)
+    end1=time.time()
+    T2=end1-start1
+    print("El tiempo por columnas sin mutiprocessing es;"+ str(T2))
+    
+    start1=time.time()
     for i in range(0, 2):
         p1=multiprocessing.Process(target=prod_filas, args=(a,b,))
         p1.start()
@@ -65,9 +86,18 @@ if __name__ == '__main__':
     
     
 
-    end2=time.time()
-    T2=end2-start2
-    print("El tiempo con mutiprocessing es;"+ str(T2))
+    end1=time.time()
+    T3=end1-start1
+    print("El tiempo por filas con mutiprocessing es;"+ str(T3))
     
+    start1=time.time()
+    for i in range(0, 2):
+        p1=multiprocessing.Process(target=prod_columnas, args=(aa,bb,))
+        p1.start()
+    p1.join()
+    
+    end1=time.time()
+    T4=end1-start1
+    print("El tiempo por columnas con mutiprocessing es;"+ str(T4))
     
     
